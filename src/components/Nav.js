1ea7aff4970559f7f874c959ae5d321ae2813b7c
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-03 06:02:18 +0700

    icon added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,17 +10,20 @@ const Nav = () => {
       <div className="container mx-auto">
         {/* nav inner */}
         <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50 ">
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link to="home" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
             <BiHomeAlt />
           </Link>
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link to="about" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
             <BiUser />
           </Link>
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link to="services" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
             <BsClipboardData />
           </Link>
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link to="work" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
             <BsBriefcase />
+          </Link>
+          <Link to="contact" className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+            <BsChatSquare />
           </Link>
         </div>
       </div>
