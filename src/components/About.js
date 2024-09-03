73a7281bb944bc916e61a83b72595a7bb39d1c07
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-03 11:16:29 +0700

    import countup, useinview, motion, variants

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,13 @@
 import React from 'react';
+// countup
+import CountUp from 'react-countup';
+// intersection observer hook
+import { useInView } from 'react-intersection-observer';
+// motion
+import { motion } from 'framer-motion';
+// variants
+import { fadeIn } from '../variants';
+
 
 const About = () => {
   return <div id='about' className='section'>About</div>;
