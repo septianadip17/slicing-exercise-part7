22b035bf0a8252859421a6182758cfa66cac1603
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-03 07:31:56 +0700

    header and home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+// images
+import logo from "../assets/logo.svg";
 
 const Header = () => {
-  return <div className='bg-pink-300'>Header</div>;
+  return (
+    <header className="py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          {/* logo */}
+          <a href="/">
+            <img src={logo} alt="logo" />
+          </a>
+          {/* button */}
+          <button className="btn btn-sm">Work with me</button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
